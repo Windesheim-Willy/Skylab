db6557d83517be531fcd9b8cc3b2ce5574059d35
mvwm <marc@vanwaltmeijer.nl>
2018-10-26 22:32:48 +0200

    Done, incl gebruik van willy.ini

--- Testmail_gmail.py
+++ Testmail_gmail.py
@@ -1,61 +1,21 @@
-# import smtplib
-# fromaddr = 'willythegarbagedisposal@gmail.com'
-# toaddrs  = 's1096820@windesheim.nl'
-# msg = 'Why,Oh why!'
-# username = 'willythegarbagedisposal@gmail.com'
-# password = 'Willy1234'
-#
-# msg = "\r\n".join([
-#   "From: willythegarbagedisposal@gmail.com",
-#   "To: s1096820@windesheim.nl",
-#   "Subject: Testbericht",
-#   "",
-#   "Hierbij een testbericht van Willi. Groet, Marc"
-#   ])
-#
-# server = smtplib.SMTP('smtp.gmail.com:587')
-# server.ehlo()
-# server.starttls()
-# server.login(username,password)
-# server.sendmail(fromaddr, toaddrs, msg)
-# server.quit()
+import configparser
+import smtplib
 
-#!/usr/bin/python
+config = configparser.ConfigParser()
+config.read('willy.ini')
 
-#from smtplib import SMTP # Standard connection
-from smtplib import SMTP_SSL as SMTP #SSL connection
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEText import MIMEText
+sender = config['DEFAULT']['GMAIL'] # Willy Gmail account
+sender_ww = config['DEFAULT']['GMAIL-WW'] # Willy Gmail wachtwoord
 
-sender = 'willythegarbagedisposal@gmail.com'
-receivers = ['s1096820@windesheim.nl']
+lijst = ['dylan.reimerink@windesheim.nl', 'fabian.vande.bor@windesheim.nl', 'george.wassink@windesheim.nl', 'johnny.borg@windesheim.nl', 'michel.stompe@windesheim.nl', 'sven.pook@windesheim.nl', 'jan-gerrit.elzinga@windesheim.nl', 'johan.int.hout@windesheim.nl', 'marc.van.walt-meijer@windesheim.nl', 'ruben.stuut@windesheim.nl']
 
-print(sender)
+# Config in gmail acoount Inloggen en beveiliging / Apps met toegang tot je account / Apps met lagere beveiliging toestaan: AAN
 
-msg = MIMEMultipart()
-msg['From'] = 'willythegarbagedisposal@gmail.com'
-msg['To'] = 's1096820@windesheim.nl'
-msg['Subject'] = 'simple email via python test 1'
-message = 'This is the body of the email line 1\nLine 2\nEnd'
-msg.attach(MIMEText(message))
-
-ServerConnect = False
-try:
-    smtp_server = SMTP('smtp.gmail.com','465')
-    smtp_server.login('willythegarbagedisposal@gmail.com', 'Willy1234')
-    ServerConnect = True
-except SMTPHeloError as e:
-    print ("Server did not reply")
-except SMTPAuthenticationError as e:
-    print ("Incorrect username/password combination")
-except SMTPException as e:
-    print ("Authentication failed")
-
-if ServerConnect == True:
-    try:
-        smtp_server.sendmail(sender, receivers, msg.as_string())
-        print ("Successfully sent email")
-    except SMTPException as e:
-        print ("Error: unable to send email"), e
-    finally:
-        smtp_server.close()
+for i in range(len(lijst)):
+    gserver = smtplib.SMTP('smtp.gmail.com', 587)
+    gserver.ehlo()
+    gserver.starttls()
+    gserver.login(sender, sender_ww)
+    gserver.sendmail(sender, lijst[i], 'Subject:Testmail van Willy\nSpammen vanuit Python werkt!\n\nGroet,\nMarc.')
+    gserver.quit()
+    print('Bericht verzonden aan ', lijst[i])
