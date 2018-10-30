dcc6c4ab69fb1e36d532ddbe8d124de7a35056fc
mvwm <marc@vanwaltmeijer.nl>
2018-10-30 11:23:34 +0100

    Changed to English

--- db_connectie.py
+++ db_connectie.py
@@ -1,6 +1,6 @@
-#Query uitvoeren op de database vanuit voorbewerkte SQL opdracht
+# Make connection tot PostgreSQL database
 db_string = 'postgresql+psycopg2://postgres:postgres@192.168.1.2/Yes'
-# Maak connectie naar database met juiste string
+# Make connection to database with correct string
 conn = create_engine(db_string)
-#Query uitvoeren op de database
+# Execute query on database
 result = pd.read_sql_query('select * from marc_regressie',con=conn)
