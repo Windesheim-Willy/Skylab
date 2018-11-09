b1f64d7471593d500810c321d48948b7ed8519d8
mvwm <31545645+mvwm@users.noreply.github.com>
2018-11-09 23:21:34 +0100

    Delete db_connectie.py

--- db_connectie.py
+++
@@ -1,6 +0,0 @@
-# Make connection tot PostgreSQL database
-db_string = 'postgresql+psycopg2://postgres:postgres@192.168.1.2/Yes'
-# Make connection to database with correct string
-conn = create_engine(db_string)
-# Execute query on database
-result = pd.read_sql_query('select * from marc_regressie',con=conn)
