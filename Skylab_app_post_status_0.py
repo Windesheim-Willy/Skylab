2926452a6fb789e8110d0c916d6df04bfe5aa06e
mvwm <31545645+mvwm@users.noreply.github.com>
2018-11-27 20:47:01 +0100

    Update Skylab_app_post_status_0.py

--- Skylab_app_post_status_0.py
+++ Skylab_app_post_status_0.py
@@ -9,17 +9,40 @@ import rospy
 from std_msgs.msg import Int32
 from std_msgs.msg import String
 import pandas as pd
+import configparser
+from sqlalchemy import create_engine
+import psycopg2
+
+config = configparser.ConfigParser()
+config.read('willy.ini')
+#
+postgresql_pwd = config['POSTGRESQL']['PASSWORD_POSTGRESQL'] # PostgreSQL password
+#
+# # Make connection tot PostgreSQL database
+#
+db_string = 'postgresql+psycopg2://postgres:' + postgresql_pwd + '@10.10.1.35/Willy'
+# # Make connection to database with correct string
+#
+conn = create_engine(db_string)
+
+translate_table  = pd.read_sql_query('select * from translate_table',con=conn)
+
+print(translate_table)
 
 # Usage: Post status from Willy activity topic to App website every 10 seconds
 willy_topic_name01 ='/navigation/current_apriltag'
 willy_topic_name02 ='/willy/activity'
 
-c_classroom = ""
+c_apriltag = ""
 c_activity = ""
 
-def callback01(msg):
-    c_classroom = msg.data
-    # To send data about status of Willy
+def callback01(msg):     # To send data about status of Willy because AprilTag changed
+    c_apriltag = msg.data
+    # translate c_apriltag to corresponding classroom using translate_table
+    translate_table_select = translate_table.loc[translate_table['apriltag'] == c_apriltag]
+    translate_table_select_index0 = translate_table_select.reset_index()
+    c_classroom = translate_table_select_index0.at[0,'classroom']
+
     encoded_body = json.dumps({
         "Locatie": c_classroom,
         "Status": c_activity,
@@ -32,9 +55,9 @@ def callback01(msg):
                      body=encoded_body)
     print(r.read())
 
-def callback02(msg):
-    c_activity = msg.data
-    # To send data about status of Willy
+def callback02(msg):     # To send data about status of Willy because activity changed
+    c_activity = str(msg.data)
+
     encoded_body = json.dumps({
         "Locatie": c_classroom,
         "Status": c_activity,
