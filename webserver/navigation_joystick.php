d0ce55964cf0af1a57a1237be3f8abdc25dde860
mvwm <31545645+mvwm@users.noreply.github.com>
2018-12-02 00:41:17 +0100

    Create navigation_joystick.php

---
+++ webserver/navigation_joystick.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<?php
+// Begin define variables about the table ----------------------------------------------------
+$page_title = "Joystick interactie";
+$table_name = "navigation_joystick";
+$number_of_records_per_page = 20;
+// End define variables about the table ----------------------------------------------------
+include("make_web_page.php");
+?>
