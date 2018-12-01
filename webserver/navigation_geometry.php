3168a7930852cc8a2959fe71957d9905f4fc853b
mvwm <31545645+mvwm@users.noreply.github.com>
2018-12-02 00:40:46 +0100

    Create navigation_geometry.php

---
+++ webserver/navigation_geometry.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<?php
+// Begin define variables about the table ----------------------------------------------------
+$page_title = "Geometrie van de navigatie";
+$table_name = "navigation_geometry";
+$number_of_records_per_page = 20;
+// End define variables about the table ----------------------------------------------------
+include("make_web_page.php");
+?>
