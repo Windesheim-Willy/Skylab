61cc91d236caba9c33b8da71941593c50649c46d
mvwm <31545645+mvwm@users.noreply.github.com>
2018-12-02 00:39:38 +0100

    Create app_user_location.php

---
+++ webserver/app_user_location.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<?php
+// Begin define variables about the table ----------------------------------------------------
+$page_title = "Gebruikersverzoeken";
+$table_name = "app_user_location";
+$number_of_records_per_page = 20;
+// End define variables about the table ----------------------------------------------------
+include("make_web_page.php");
+?>
