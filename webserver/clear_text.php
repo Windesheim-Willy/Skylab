895f8711909072274ad3eabe08432ef4b3a33d4f
mvwm <31545645+mvwm@users.noreply.github.com>
2018-12-02 00:40:13 +0100

    Create clear_text.php

---
+++ webserver/clear_text.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<?php
+// Begin define variables about the table ----------------------------------------------------
+$page_title = "Gesproken tekst";
+$table_name = "interaction_clear_text";
+$number_of_records_per_page = 20;
+// End define variables about the table ----------------------------------------------------
+include("make_web_page.php");
+?>
