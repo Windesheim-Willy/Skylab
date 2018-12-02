60072a688ff21b41ecfcdee44906e973cd3eea13
mvwm <31545645+mvwm@users.noreply.github.com>
2018-12-02 21:38:56 +0100

    Update navigation_joystick.php

--- webserver/navigation_joystick.php
+++ webserver/navigation_joystick.php
@@ -3,7 +3,6 @@
 // Begin define variables about the table ----------------------------------------------------
 $page_title = "Joystick interactie";
 $table_name = "navigation_joystick";
-$number_of_records_per_page = 20;
 // End define variables about the table ----------------------------------------------------
 include("make_web_page.php");
 ?>
