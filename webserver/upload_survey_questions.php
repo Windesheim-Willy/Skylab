f9f7ffe3443af4af737c7d76023511e2eeb3874c
mvwm <31545645+mvwm@users.noreply.github.com>
2018-12-02 20:50:28 +0100

    Update upload_survey_questions.php

--- webserver/upload_survey_questions.php
+++ webserver/upload_survey_questions.php
@@ -2,6 +2,7 @@
 <?php
 // make SSH connection
 print("Making SSH connection <br>");
+// use correct host when in production
 $connection = ssh2_connect('10.10.1.34',22);
 
 include("secret_info.php");
