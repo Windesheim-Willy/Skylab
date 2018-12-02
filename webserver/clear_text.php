2d02a6800c72dc37df73569a93343e561ac756bc
mvwm <31545645+mvwm@users.noreply.github.com>
2018-12-02 21:34:22 +0100

    Update clear_text.php

--- webserver/clear_text.php
+++ webserver/clear_text.php
@@ -3,7 +3,6 @@
 // Begin define variables about the table ----------------------------------------------------
 $page_title = "Gesproken tekst";
 $table_name = "interaction_clear_text";
-$number_of_records_per_page = 20;
 // End define variables about the table ----------------------------------------------------
 include("make_web_page.php");
 ?>
