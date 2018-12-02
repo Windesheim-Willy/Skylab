ce6a27cb11920df4bd5111ef25bd5cf53decb3f2
mvwm <31545645+mvwm@users.noreply.github.com>
2018-12-02 21:38:34 +0100

    Update navigation_geometry.php

--- webserver/navigation_geometry.php
+++ webserver/navigation_geometry.php
@@ -3,7 +3,7 @@
 // Begin define variables about the table ----------------------------------------------------
 $page_title = "Geometrie van de navigatie";
 $table_name = "navigation_geometry";
-$number_of_records_per_page = 20;
 // End define variables about the table ----------------------------------------------------
 include("make_web_page.php");
 ?>
+
