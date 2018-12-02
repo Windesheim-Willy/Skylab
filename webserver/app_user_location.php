ec20fbe8675d8873b1132c2867c497d2b7cbd7a4
mvwm <31545645+mvwm@users.noreply.github.com>
2018-12-02 21:33:17 +0100

    Update app_user_location.php

--- webserver/app_user_location.php
+++ webserver/app_user_location.php
@@ -3,7 +3,6 @@
 // Begin define variables about the table ----------------------------------------------------
 $page_title = "Gebruikersverzoeken";
 $table_name = "app_user_location";
-$number_of_records_per_page = 20;
 // End define variables about the table ----------------------------------------------------
 include("make_web_page.php");
 ?>
