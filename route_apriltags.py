3a4e065e9180fbf22c6754c8168767bf9210e96c
MFS2018 <45394297+MFS2018@users.noreply.github.com>
2018-12-04 15:14:52 +0100

    Add files via upload

---
+++ route_apriltags.py
@@ -0,0 +1,26 @@
+## start python
+Python
+import psycopg2
+import pandas as pd
+from sqlalchemy import create_engine
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+## connect to database
+db_string = 'postgresql+psycopg2://postgres:willy@10.10.1.35/Willy'
+conn = create_engine(db_string)
+
+## select navigation_apriltag 
+willy_route = pd.read_sql_query('Select * from navigation_apriltag WHERE apriltag <>0 ',con=conn)
+
+## plot counts
+counts = willy_route['apriltag'].value_counts().sort_index()
+pd.value_counts(willy_route['apriltag']).plot.bar()
+plt.show()
+
+
+
+
+
+
+
