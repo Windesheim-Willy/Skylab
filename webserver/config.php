1e2a777de3c5c4641e55aa7fb824e905dd71b58e
mvwm <31545645+mvwm@users.noreply.github.com>
2018-12-11 17:55:14 +0100

    Update config.php

--- webserver/config.php
+++ webserver/config.php
@@ -2,6 +2,6 @@
 // Number of lines per pages when showing topic info
 define('LINES_PER_PAGE', 20);
 // SI node IP address for survey exchange
-define('SI_SURVEY_NODE_IP_ADDRESS', "10.10.1.34");
+define('SI_SURVEY_NODE_IP_ADDRESS', "192.168.0.11");
 ?>
 
